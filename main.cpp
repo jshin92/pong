769d79a0d27b374477b58ebe81d1f55e5def8760
joe <joe@belgarath>
2013-02-08 01:35:32 -0800

    added collision logic for ball against walls

--- main.cpp
+++ main.cpp
@@ -12,10 +12,16 @@
 #include <iostream>
 #define WIDTH 800
 #define HEIGHT 600
+#define START_X_VELO 5
+#define START_Y_VELO 2
 int main()
 {
 	// create main window
 	sf::RenderWindow App(sf::VideoMode(WIDTH, HEIGHT), "Pong!");	
+	int xVel = START_X_VELO;
+	int yVel = START_Y_VELO;
+	int x = WIDTH/2;
+	int y = HEIGHT/2;
 
 	// start game loop
 	while (App.IsOpened())
@@ -27,9 +33,15 @@ int main()
 				App.Close();
 
 		App.Clear();
-		App.Draw(sf::Shape::Circle(WIDTH/2, HEIGHT/2, 100, sf::Color::Yellow, 10, 
+		App.Draw(sf::Shape::Circle(x, y, 50, sf::Color::Yellow, 10, 
 						 sf::Color::Blue));
 		App.Display();
+		x += xVel;
+		y += yVel;
+		if ( x > WIDTH || x < 0)
+			xVel = -xVel;
+		if ( y > HEIGHT || y < 0)
+			yVel = -yVel;
 
 	}
 
